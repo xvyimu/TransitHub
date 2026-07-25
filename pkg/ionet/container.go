e474c660113f12e7d01dc5879af12ac9653c5e74
xvyimu <179692766+xvyimu@users.noreply.github.com>
2026-07-25 16:43:56 +0800

    refactor(ionet): use common JSON helpers

--- pkg/ionet/container.go
+++ pkg/ionet/container.go
@@ -1,12 +1,12 @@
 package ionet
 
 import (
-	"encoding/json"
 	"fmt"
 	"strings"
 	"time"
 
 	"github.com/samber/lo"
+	"github.com/xvyimu/TransitHub/common"
 )
 
 // ListContainers retrieves all containers for a specific deployment
@@ -290,7 +290,7 @@ func (c *Client) ExecuteInContainer(deploymentID, containerID string, command []
 	}
 
 	var result map[string]interface{}
-	if err := json.Unmarshal(resp.Body, &result); err != nil {
+	if err := common.Unmarshal(resp.Body, &result); err != nil {
 		return "", fmt.Errorf("failed to parse execution result: %w", err)
 	}
 
